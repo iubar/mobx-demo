5daaf60a08923b76efc042e91e30f0ad6ee6cb0a
Daniele <daniele.montesi@gmail.com>
2020-10-07 15:35:25 +0200

    Update mobx

--- src/stores/Store.js
+++ src/stores/Store.js
@@ -3,88 +3,94 @@ import Constants from 'expo-constants';
 
  class Store {
 
-  @observable name = 'MobX Demo';
+    // Observable attributes
+    name = 'MobX Demo';
+    buttonEnabled = true;
+    data = { results: [] };
 
-  @observable buttonEnabled = true;
+    // Not observable attributes
+    text = '';
 
-  // An observable to save image response from api
-  @observable data = {
-    results: []
-  };
+    constructor() {
+        // makeObservable(this)
+        makeObservable(this, {
+            name: observable,
+            buttonEnabled: observable,
+            data: observable,
 
-  // The following attribute is not observable
-  text = '';
-
-  // The values that can be derived from already defined observables are computed values.
-  // Computed works like a getter function to get derived state from the observable
-  // The following computed function doesn't work as expected because 'text' is not observable
-  // This is right because, right now, we don't need 'text' to be observable
-  @computed
-  get searchedText(){
-    let _text = 'undefined';
-    if(this.text){
-      _text = 'You have searched the string: ' + this.text;
-    }
-    return _text;
-  } 
-
-  // Actions are simply functions that modify the state.
-  @action 
-  updateText = (text) => {
-      console.log('action called, text updated : ' + text);
-      this.text = text;
+            updateText: action, 
+            setData: action, 
+            setButtonEnabled: action, 
+            searchImages: action,
+            
+            searchedText: computed
+        })
     }
 
-  // Observables can be modifies by an action only.
-  // Actions are simply functions that modify the state.
-  @action  
-  setData = (json) => {
-    console.log('data updated');
-    this.data.results = json.results;
-    console.log('this.data size: ' + this.data.results.length);
-  };
-
-  @action  
-  setButtonEnabled = (b) => {
-    this.buttonEnabled = b;
-  };  
- 
-  // An action to call API and search images
-  @action
-  searchImages = async () => {
-    this.setButtonEnabled(false);
-    let API_KEY = Constants.manifest.extra.unsplashApiKey;
-    let page = 1; // vale sempre 1 in questo esempio
-    let per_page = 2;
-    let url = 'https://api.unsplash.com';
-    let lang = 'en';
-    let orientation = 'landscape';
-    url = url + '/search/photos';
-    url = url + '?page=' + page + '&per_page=' + per_page + '&lang=' + lang + '&orientation=' + orientation + '&query=' + encodeURIComponent(this.text) ;
-    console.log('Fetching ' + url);
-    let result = await fetch(url, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
-          'Accept-Version': 'v1',
-          Authorization: 'Client-ID ' + API_KEY,        
+    // The values that can be derived from already defined observables are computed values.
+    // Computed works like a getter function to get derived state from the observable
+    // The following computed function doesn't work as expected because 'text' is not observable
+    // This is right because, right now, we don't need 'text' to be observable
+    get searchedText(){
+        let _text = 'undefined';
+        if(this.text){
+            _text = 'You have searched the string: ' + this.text;
         }
-    });	
-  let json = await result.json();
-  //console.log('json: ' + JSON.stringify(json));		
-  const statusCode = result.status;             
-  if (statusCode != 200){
-    console.log('Http error: ' + statusCode);			
-  }else{			
-    console.log('Ok: ' + statusCode);
-    console.log('results size: ' + json.results.length);
-    this.setData(json);
-  }
-  this.setButtonEnabled(true);
-}
+        
+        return _text;
+    }
 
+    // Actions are simply functions that modify the state.
+    updateText = (text) => {
+        console.log('action called, text updated : ' + text);
+        this.text = text;
+    }
+
+    // Observables can be modifies by an action only.
+    // Actions are simply functions that modify the state.
+    setData = (json) => {
+        console.log('data updated');
+        this.data.results = json.results;
+        console.log('this.data size: ' + this.data.results.length);
+    };
+
+    setButtonEnabled = (b) => {
+        this.buttonEnabled = b;
+    };  
  
+    // An action to call API and search images
+    searchImages = async () => {
+        this.setButtonEnabled(false);
+        let API_KEY = Constants.manifest.extra.unsplashApiKey;
+        let page = 1; // vale sempre 1 in questo esempio
+        let per_page = 2;
+        let url = 'https://api.unsplash.com';
+        let lang = 'en';
+        let orientation = 'landscape';
+        url = url + '/search/photos';
+        url = url + '?page=' + page + '&per_page=' + per_page + '&lang=' + lang + '&orientation=' + orientation + '&query=' + encodeURIComponent(this.text) ;
+        console.log('Fetching ' + url);
+        let result = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+                'Accept-Version': 'v1',
+                Authorization: 'Client-ID ' + API_KEY,        
+            }
+        });	
+        let json = await result.json();
+        //console.log('json: ' + JSON.stringify(json));		
+        const statusCode = result.status;             
+        if (statusCode != 200){
+            console.log('Http error: ' + statusCode);			
+        } else {
+            console.log('Ok: ' + statusCode);
+            console.log('results size: ' + json.results.length);
+            this.setData(json);
+        }
+        this.setButtonEnabled(true);
+    }
 
 /*
 
@@ -119,10 +125,6 @@ decorate(Store, {
 });
 */
 
-
-constructor() {
-  makeObservable(this)
-}
 
 
 } // end class
